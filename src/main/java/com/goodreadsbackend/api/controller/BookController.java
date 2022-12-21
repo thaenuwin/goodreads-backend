c996e5dbe308762ea095b0b8b6e1914b0c0861c0
thaenuwinmmk@gmail.com <thaenuwinmmk@gmail.com>
2022-12-21 21:45:03 +0630

    add logger with log4 2j

--- src/main/java/com/goodreadsbackend/api/controller/BookController.java
+++ src/main/java/com/goodreadsbackend/api/controller/BookController.java
@@ -5,6 +5,7 @@ import com.goodreadsbackend.api.opr.BookCreation;
 import com.goodreadsbackend.api.util.ResponseMessageUtil;
 import com.goodreadsbackend.api.util.search.QueryBook;
 import com.goodreadsbackend.api.util.search.comp.QueryParam;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
+@Log4j2
 public class BookController {
 
     @Autowired
@@ -36,6 +38,7 @@ public class BookController {
     @PostMapping(value = "/book-data")
     public ResponseEntity<Object> findPanelAssignmentData(
             @RequestHeader(value = "Authorization") String authorization, @RequestBody() QueryParam queryCmd) {
+        log.debug("Query book data param : ", queryCmd);
         return ResponseEntity.ok(queryBook.query(queryCmd));
     }
 }
